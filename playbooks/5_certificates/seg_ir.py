97825c031375d20a85acd514877468816b2cd1fc
Kim Frellsen <kim@frellsen.se>
2024-04-30 17:29:44 +0200

    Sanetized input and made DoS policies better

--- playbooks/5_certificates/seg_ir.py
+++ playbooks/5_certificates/seg_ir.py
@@ -14,23 +14,39 @@ def read_command_from_file(file_path):
 # def load yaml file
 def open_yaml_vars(yaml_file_path):
     with open(yaml_file_path, "r") as file:
-        yaml_data = yaml.safe_load(file)
-    return yaml_data
+        _yaml_data = yaml.safe_load(file)
+    return _yaml_data
 
 def run_command_file(host, command_file, ssh_port, username, password):
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(host, ssh_port, username, password)
+    _client = paramiko.SSHClient()
+    _client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    _client.connect(host, ssh_port, username, password)
+    _output = ""
+    _allowed_to_exec = True
 
-    ## read file
-    command = read_command_from_file(command_file)
-    _stdin, _stdout, _stderr = client.exec_command(command)
-    output = _stdout.read().decode()
-    
-    client.close()
-    del _stdin, _stdout, _stderr
-    
-    return output
+    # read file
+    commands = read_command_from_file(command_file)
+
+    #sanitize input
+    disallowed_commands = ["factoryreset", "factoryreset2", "shutdown", "reboot", "delete", "purge", "set"]
+    commands_split = commands.split('\n')
+
+    # Check if the command is in the disallowed commands list
+    for cmd in commands_split:
+       cmd = cmd.strip()
+       if any(dcmd in cmd for dcmd in disallowed_commands):
+         print(f"command file '{command_file}' contains the disallowed command '{cmd}'. Commands are not run.")
+         _allowed_to_exec = False
+
+    # send command to _client
+    if _allowed_to_exec:
+        _stdin, _stdout, _stderr = _client.exec_command(commands)
+        _output = _stdout.read().decode()
+        del _stdin, _stdout, _stderr
+
+    _client.close()
+
+    return _output
 
 
 # read vars.yml
